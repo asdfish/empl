2e1a29aa907a11c4cf5d632156599c9f4e5ff7ab
asdfish <lgray3420@gmail.com>
2025-05-05 12:58:56 -0400

    use c style entry point

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+#![cfg_attr(not(test), no_main)]
+
+use std::ffi::{c_char, c_int};
+
+#[cfg_attr(not(test), unsafe(no_mangle))]
+extern "system" fn main(_: *const *const c_char, _: c_int) -> c_int {
+    0
 }
