fbc9e6d9af4c0dfd113764b76ad6e5cbeef674a6
asdfish <lgray3420@gmail.com>
2025-05-17 11:58:53 -0400

    allow repl to print ast

--- src/bin/repl.rs
+++ src/bin/repl.rs
@@ -1,17 +1,73 @@
+#![cfg_attr(not(test), no_main)]
+
+#[path = "./repl/config.rs"]
+pub mod config;
+
 use {
-    empl::config::clisp::{lexer::LexemeParser, parser::Parser},
-    std::io::stdin,
+    crate::config::{
+        Config,
+        Stage,
+    },
+    empl::{
+        argv::Argv,
+        config::clisp::{ast::ExprParser, lexer::LexemeParser, parser::{Parser, ParserOutput}},
+        flag::Arguments,
+    },
+    std::{
+        ffi::{
+            c_char,
+            c_int,
+        },
+        io::stdin,
+    },
 };
 
-fn main() {
+#[cfg_attr(not(test), unsafe(no_mangle))]
+fn main(argc: c_int, argv: *const *const c_char) -> c_int {
+    let argv = match unsafe { Argv::new(argc, argv) } {
+        Ok(a) => a,
+        Err(err) => {
+            eprintln!("{err}");
+            return 1;
+        }
+    };
+
+    let config = match Config::new(Arguments::new(argv.skip(1))) {
+        Ok(c) => c,
+        Err(err) => {
+            eprintln!("{err}");
+            return 1;
+        }
+    };
+    println!("{:?}", config);
+
     let mut buf = String::new();
-    while stdin().read_line(&mut buf).is_ok() {
-        let mut next = buf.as_str();
-        while let Ok(output) = LexemeParser.parse(next) {
-            println!("{:?}", output.output);
-            next = output.next;
+    loop {
+        buf.clear();
+        if let Err(err) = stdin().read_line(&mut buf) {
+            eprintln!("failed to read line: {err}");
+            break 1;
         }
 
-        buf.clear();
+        let mut input = buf.as_str();
+        let mut lexemes = Vec::new();
+
+        while let Ok(ParserOutput { next, output, .. }) = LexemeParser.parse(input) {
+            lexemes.push(output);
+            input = next;
+        }
+
+        if config.stage == Stage::Lex {
+            println!("{lexemes:?}");
+            continue;
+        }
+
+        let Ok(ast) = ExprParser.parse(&lexemes) else {
+            continue
+        };
+        if config.stage == Stage::Parse {
+            println!("{ast:?}");
+            continue;
+        }
     }
 }
