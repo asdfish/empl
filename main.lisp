f0d6cbf8b21d04f34e83b551a205a89784aa73e4
asdfish <lgray3420@gmail.com>
2025-05-22 16:23:38 -0400

    improve configuration

--- main.lisp
+++ main.lisp
@@ -1,2 +1,4 @@
 (progn
-  (set-cfg! "cursor-colors" (list "black" "white")))
+  (set-cfg! "cursor-colors" (list "black" "white"))
+  (set-cfg! "menu-colors" (list "white" "black"))
+  (set-cfg! "selection-colors" (list "red" "none")))
