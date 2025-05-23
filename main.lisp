6dbc245036e6f94cc936502a15d8d8706710a6dd
asdfish <lgray3420@gmail.com>
2025-05-23 15:47:50 -0400

    improve example config

--- main.lisp
+++ main.lisp
@@ -1,4 +1,13 @@
 (progn
   (set-cfg! "cursor-colors" (list "black" "white"))
   (set-cfg! "menu-colors" (list "white" "black"))
-  (set-cfg! "selection-colors" (list "red" "none")))
+  (set-cfg! "selection-colors" (list "red" "none"))
+  (let ((music-dir (path (concat (env "HOME") (path-separator) "files" (path-separator) "music")))
+	(playlist-paths (seq-filter path-is-dir (path-children music-dir)))
+	(playlists (seq-map
+		    (lambda (playlist)
+		      (list
+		       (path-name playlist)
+		       (seq-filter path-is-file (path-children playlist))))
+		    playlist-paths)))
+    (set-cfg! "playlists" playlists))
