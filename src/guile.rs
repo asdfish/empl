b7d15a0f5c52f2d269be103255673852d5a7a679
asdfish <lgray3420@gmail.com>
2025-07-25 13:37:44 -0400

    merge all scheme files

--- src/guile.rs
+++ src/guile.rs
@@ -17,19 +17,17 @@
 // along with empl.  If not, see <http://www.gnu.org/licenses/>.
 
 use {
+    crate::guile,
     parking_lot::Mutex,
     std::{
-        ffi::{CStr, c_void},
+        ffi::{CStr, c_int, c_void},
+        marker::PhantomData,
         ptr,
         sync::atomic::{self, AtomicBool},
     },
 };
 
-mod define_fn;
-mod with_guile;
-mod without_guile;
-
-pub use {crate::guile::with_guile::with_guile, proc_macros::guile_fn};
+pub use proc_macros::guile_fn;
 
 /// Lock for thread initialization
 static INIT_LOCK: Mutex<()> = const { Mutex::new(()) };
@@ -42,6 +40,30 @@ thread_local! {
 }
 
 pub struct Api(());
+impl Api {
+    pub fn define_fn<F>(&self)
+    where
+        F: GuileFn,
+    {
+        unsafe {
+            guile::sys::scm_c_define_gsubr(
+                F::NAME.as_ptr(),
+                F::REQUIRED as c_int,
+                F::OPTIONAL as c_int,
+                F::REST.into(),
+                F::DRIVER,
+            );
+        }
+    }
+
+    pub fn make_string<S>(string: &S) -> Scm
+    where
+        S: AsRef<str> + ?Sized,
+    {
+        let string = string.as_ref();
+        Scm::new(unsafe { guile::sys::scm_from_utf8_stringn(string.as_ptr().cast(), string.len()) })
+    }
+}
 
 struct GuileModeToggleData<F, O> {
     operation: Option<F>,
@@ -91,6 +113,75 @@ trait GuileModeToggle<F, O> {
     }
 }
 
+struct WithGuile<F, O>
+where
+    F: FnOnce(&mut guile::Api) -> O,
+{
+    _marker: PhantomData<(F, O)>,
+}
+impl<F, O> GuileModeToggle<F, O> for WithGuile<F, O>
+where
+    F: FnOnce(&mut guile::Api) -> O,
+{
+    const GUILE_MODE_STATE: bool = true;
+    const SCM_FN: unsafe extern "C" fn(
+        _: Option<unsafe extern "C" fn(_: *mut c_void) -> *mut c_void>,
+        *mut c_void,
+    ) -> *mut c_void = guile::sys::scm_with_guile;
+
+    fn execute(operation: F) -> O {
+        operation(&mut guile::Api(()))
+    }
+}
+
+pub fn with_guile<F, O>(operation: F) -> O
+where
+    F: FnOnce(&mut guile::Api) -> O,
+{
+    if GUILE_MODE.with(|mode| mode.load(atomic::Ordering::Acquire)) {
+        operation(&mut guile::Api(()))
+    } else {
+        let _lock = INIT
+            .with(|init| !init.load(atomic::Ordering::Acquire))
+            .then(|| INIT_LOCK.lock());
+
+        WithGuile::call(|api| {
+            INIT.with(|init| init.store(true, atomic::Ordering::Release));
+            operation(api)
+        })
+    }
+}
+
+struct WithoutGuile<F, O>
+where
+    F: FnOnce() -> O,
+{
+    _marker: PhantomData<(F, O)>,
+}
+impl<F, O> GuileModeToggle<F, O> for WithoutGuile<F, O>
+where
+    F: FnOnce() -> O,
+{
+    const GUILE_MODE_STATE: bool = false;
+    const SCM_FN: unsafe extern "C" fn(
+        _: Option<unsafe extern "C" fn(_: *mut c_void) -> *mut c_void>,
+        *mut c_void,
+    ) -> *mut c_void = guile::sys::scm_without_guile;
+
+    fn execute(operation: F) -> O {
+        operation()
+    }
+}
+
+impl guile::Api {
+    pub fn without_guile<F, O>(&mut self, operation: F) -> O
+    where
+        F: FnOnce() -> O,
+    {
+        WithoutGuile::call(operation)
+    }
+}
+
 #[derive(Clone, Copy, Debug)]
 #[repr(C)]
 pub struct Scm(sys::SCM);
@@ -120,7 +211,14 @@ pub mod sys {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use {
+        super::*,
+        crate::{
+            guile::{Scm, guile_fn},
+            tests::ENV_VAR_LOCK,
+        },
+        std::sync::atomic::{self, AtomicBool},
+    };
 
     #[test]
     fn guile_fn_impl() {
@@ -150,5 +248,58 @@ mod tests {
         assert_eq!(Baz::OPTIONAL, 0);
         assert_eq!(Baz::REST, true);
         assert_eq!(Baz::NAME, c"baz");
+    }
+
+    #[cfg_attr(miri, ignore)]
+    #[test]
+    fn define_fn() {
+        let _lock = ENV_VAR_LOCK.read();
+
+        static EXECUTED: AtomicBool = AtomicBool::new(false);
+
+        #[guile_fn]
+        fn set_executed([x]: [Scm; 1], _: [Option<Scm>; 0]) -> Scm {
+            EXECUTED.store(true, atomic::Ordering::Release);
+            x
+        }
+
+        guile::with_guile(|api| {
+            api.define_fn::<SetExecuted>();
+            // TODO: implement and use a wrapper for scm_eval_string
+            unsafe {
+                guile::sys::scm_eval_string(guile::sys::scm_from_utf8_string(
+                    c"(set-executed 1)".as_ptr(),
+                ));
+            }
+        });
+
+        assert!(EXECUTED.load(atomic::Ordering::Acquire));
+    }
+
+    #[cfg_attr(miri, ignore)]
+    #[test]
+    fn with_guile_nesting() {
+        let _lock = ENV_VAR_LOCK.read();
+        assert!(with_guile(|_| with_guile(|_| true)));
+    }
+
+    #[cfg_attr(miri, ignore)]
+    #[test]
+    fn multi_threaded_init() {
+        let _lock = ENV_VAR_LOCK.read();
+        let spawn = || std::thread::spawn(|| with_guile(|_| {}));
+        [(); 2]
+            .map(|_| spawn())
+            .into_iter()
+            .for_each(|thread| thread.join().unwrap());
+    }
+
+    #[cfg_attr(miri, ignore)]
+    #[test]
+    fn without_guile_nesting() {
+        let _lock = ENV_VAR_LOCK.read();
+        assert!(guile::with_guile(
+            |api| api.without_guile(|| guile::with_guile(|_| true))
+        ));
     }
 }
